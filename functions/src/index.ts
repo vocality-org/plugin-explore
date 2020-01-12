1d0d7c86c83f5e24aa8c32fa9e4ffa1af43db4d0
Julian Kaindl <kaindl745@gmail.com>
2020-01-13 00:29:15 +0100

    Add function to get plugins

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,15 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest(async (req, res) => {
+    const db = admin.firestore();
+
+    const documents = await db
+        .collection('third-party-plugins')
+        .get()
+        .then(snap => {
+            snap.docs;
+        });
+
+    res.json(documents);
+});
